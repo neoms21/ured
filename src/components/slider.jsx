120ffd4ee208c873e4082b033fc32d2f83f39371
Manoj Sethi <msethi@qubittech.co.uk>
2018-04-09 10:30:26 +0100

    slider almost there

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -12,15 +12,14 @@ class Volume extends Component {
 		this.state = {
 			volume: 0,
 			position: 'absolute',
-			left: '20px'
+			left: '40px'
 		};
 	}
 
 	handleOnChange = (e) => {
-		console.log(e.target.id);
-		console.log();
+		console.log(e.target.getBoundingClientRect().left, e.currentTarget.offsetLeft);
 		this.setState({
-			left: e.target.getBoundingClientRect().left / 2
+			left: e.currentTarget.offsetLeft + 10
 		});
 	};
 	handleImageClick = () => {
@@ -30,7 +29,6 @@ class Volume extends Component {
 	};
 
 	render() {
-		console.log(reactLogo);
 		let { volume } = this.state;
 		return (
 			<div className={styles.container}>
@@ -40,19 +38,25 @@ class Volume extends Component {
 					onClick={this.handleImageClick}
 					alt="marker"
 				/>
-				<div className={styles.stepLabels}>
-					<div className={`${styles.stepLabel} ${styles['first-step']}`}>Low</div>
+				{/* <div className={styles.stepLabels}>
+					<div className={`${styles.stepLabel}`}>Low</div>
 					<div className={`${styles.stepLabel}`}>Low to Medium </div>
 					<div className={`${styles.stepLabel}`}>Medium</div>
 					<div className={`${styles.stepLabel}`}>Medium to high </div>
 					<div className={`${styles.stepLabel}`}>High </div>
-				</div>
+				</div> */}
 				<div className={styles.slider}>
-					<div id="1" className={`${styles.step}`} onClick={this.handleOnChange} />
+					{/* <div id="1" className={`${styles.step}`} onClick={this.handleOnChange} />
 					<div id="2" className={`${styles.step}`} onClick={this.handleOnChange} />
 					<div id="3" className={`${styles.step}`} onClick={this.handleOnChange} />
 					<div id="4" className={`${styles.step}`} onClick={this.handleOnChange} />
-					<div id="5" className={`${styles.step}`} onClick={this.handleOnChange} />
+                    <div id="5" className={`${styles.step}`} onClick={this.handleOnChange} /> */}
+
+					{step('1', this.handleOnChange, 'Low', styles['first-step'])}
+					{step('2', this.handleOnChange, 'Low to medium')}
+					{step('3', this.handleOnChange, 'Medium')}
+					{step('4', this.handleOnChange, 'Medium to high')}
+					{step('5', this.handleOnChange, 'High', styles['last-step'])}
 				</div>
 			</div>
 		);
@@ -60,3 +64,12 @@ class Volume extends Component {
 }
 
 export default Volume;
+
+const step = (id, onChange, label, classNames) => (
+	<div id={id} className={`${styles.step}`} onClick={onChange}>
+		<div id={styles.left} />
+		<div className={styles.divider} />
+		<span className={styles['stepLabel']}>{label}</span>
+		<div id={styles.right} />
+	</div>
+);
