0404c0fa7681300117cab715cb481920e2099c55
Manoj Sethi <msethi@qubittech.co.uk>
2018-05-22 12:08:52 +0100

    local sync

--- syncFiles.js
+++ syncFiles.js
@@ -1,17 +1,18 @@
-var dir="/Users/msethi/code/cob/src"
-var fs =require('fs');
+var dir = process.platform === 'darwin' ? "/Users/msethi/code/cob/src" : 'C:/code/ured/uploads';
+var fs = require('fs');
 var moment = require('moment');
-var afterTime = "2018-05-09T16:00:00"
+var afterTime = "2018-05-22T06:00:00"
 var path = require('path');
 var mkdirp = require('mkdirp');
-var toDir = "/Users/msethi/temp";
+var toDir = process.platform === 'darwin' ? "/Users/msethi/code/ofc/uploads" : 'C:/dev/wealth-onboarding/client/src';
 
-var getFileInfo = function (fileName) {
-  var fileTime =fs.statSync(fileName).mtime.getTime();
-  return { name:fileName,
-          dir: path.dirname(fileName),
-           changed:moment(fileTime) > moment(afterTime)
-         }
+var getFileInfo = function(fileName) {
+  var fileTime = fs.statSync(fileName).mtime.getTime();
+  return {
+    name: fileName,
+    dir: path.dirname(fileName),
+    changed: moment(fileTime) > moment(afterTime)
+  }
 }
 
 
@@ -40,21 +41,21 @@ var walk = function(dir, done) {
   });
 };
 
-var copyFiles = function(results){
-  results.forEach(f=> {
-    var correctPath = path.dirname(f.name.substr(f.name.indexOf('src')+4));
-    console.log(f.name,correctPath);
+var copyFiles = function(results) {
+  results.forEach(f => {
+    var correctPath = path.dirname(f.name.substr(f.name.indexOf('src') + 4));
+    console.log(f.name, correctPath);
     var destDir = toDir + "/" + correctPath + "/";
-    if(!fs.existsSync(destDir)){
-      mkdirp(destDir, function(err){
-        if(!err){
+    if (!fs.existsSync(destDir)) {
+      mkdirp(destDir, function(err) {
+        if (!err) {
 
           fs.copyFileSync(f.name, destDir + path.basename(f.name));
 
         }
       });
-    } else{
-        fs.copyFileSync(f.name, destDir + path.basename(f.name));
+    } else {
+      fs.copyFileSync(f.name, destDir + path.basename(f.name));
     }
   })
 }
