af8280130ca0b1bb4a926a0b0f9362ab43459b15
Manoj Sethi <msethi@qubittech.co.uk>
2018-07-29 11:19:47 +0100

    del all

--- uploads/modules/wizard/wizard-actions.js
+++
@@ -1,6 +0,0 @@
-import { CLOSE_WIZARD } from './wizard-action-types';
-export function closeWizard(){
-    return {
-        type: CLOSE_WIZARD
-    }
-}
--- uploads/validators/required.js
+++
@@ -1,6 +0,0 @@
-export const required = (value, allValues) => {
-  const valToValidate = value ? value.toString().trim() : value;
-  return valToValidate || valToValidate === 0 ? undefined : allValues;
-};
-
-export const nada = () => undefined;
